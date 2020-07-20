ce2a526e8c5d754bceaff0f8e1ae52a0d0205d75
Jesse Rappaport <jessetherappaport@gmail.com>
2020-07-20 17:13:15 -0400

    Change exports

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-module.exports.introduction = require('./introduction.md').default;
-module.exports.unit_1_chapter_1 = require('./unit-1/chapter-1.md').default;
-module.exports.unit_1_chapter_2 = require('./unit-1/chapter-2.md').default;
-module.exports.unit_1_chapter_3 = require('./unit-1/chapter-3.md').default;
-module.exports.unit_2_chapter_1 = require('./unit-2/chapter-1.md').default;
-module.exports.unit_2_chapter_2 = require('./unit-2/chapter-2.md').default;
-module.exports.unit_2_chapter_3 = require('./unit-2/chapter-3.md').default;
-module.exports.unit_3_chapter_1 = require('./unit-3/chapter-1.md').default;
-module.exports.unit_3_chapter_2 = require('./unit-3/chapter-2.md').default;
-module.exports.unit_3_chapter_3 = require('./unit-3/chapter-3.md').default;
-module.exports.unit_3_chapter_4 = require('./unit-3/chapter-4.md').default;
+module.exports.introduction = {
+  text: require('./introduction.md').default,
+  title: 'Introduction'
+};
+module.exports.unit_1_chapter_1 = {
+  text: require('./unit-1/chapter-1.md').default,
+  title: 'Logical Concepts'
+};
+module.exports.unit_1_chapter_2 = {
+  text: require('./unit-1/chapter-2.md').default,
+  title: 'Validity and Strength'
+};
+module.exports.unit_1_chapter_3 = {
+  text: require('./unit-1/chapter-3.md').default,
+  title: 'Logical Fallacies'
+};
+module.exports.unit_2_chapter_1 = {
+  text: require('./unit-2/chapter-1.md').default,
+  title: 'Symbolic Logic'
+};
+module.exports.unit_2_chapter_2 = {
+  text: require('./unit-2/chapter-2.md').default,
+  title: 'Truth Tables'
+};
+module.exports.unit_2_chapter_3 = {
+  text: require('./unit-2/chapter-3.md').default,
+  title: 'Truth Tables & Validity'
+};
+module.exports.unit_3_chapter_1 = {
+  text: require('./unit-3/chapter-1.md').default,
+  title: 'Natural Deduction 1'
+};
+module.exports.unit_3_chapter_2 = {
+  text: require('./unit-3/chapter-2.md').default,
+  title: 'Natural Deduction 2'
+};
+module.exports.unit_3_chapter_3 = {
+  text: require('./unit-3/chapter-3.md').default,
+  title: 'Natural Deduction 3'
+};
+module.exports.unit_3_chapter_4 = {
+  text: require('./unit-3/chapter-4.md').default,
+  title: 'Natural Deduction 4'
+};
